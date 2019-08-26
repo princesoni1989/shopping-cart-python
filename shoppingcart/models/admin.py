c3a1041fb4ffc7cde19f53b812c965e4f59e269b
Amulya Sharma <amulya.sharma@tothenew.com>
2019-08-26 16:27:34 +0530

    Registered models in admin

--- shoppingcart/models/admin.py
+++ shoppingcart/models/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .customers import *
+from .products import *
+from .discounts import *
+from .shipping import *
+from .orders import *
+from .payments import *
+from .wishlist import *
 
 # Register your models here.
+admin.site.register(Customers)
+admin.site.register(CustomerAddress)
+admin.site.register(SellerAddress)
+
+admin.site.register(ProductCategory)
+admin.site.register(Products)
+admin.site.register(ProductImages)
+
+admin.site.register(Discounts)
+
+admin.site.register(ShippingZones)
+admin.site.register(ShippingMethods)
+
+admin.site.register(Orders)
+
+admin.site.register(Payments)
+
+admin.site.register(Wishlist)
+admin.site.register(WishlistItems)
